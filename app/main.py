99d4d9127b5ef3e0411ebc8c5855739f747d434a
Artem Shakun <artem.shakun.91@gmail.com>
2024-06-06 18:17:50 +0300

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -7,21 +7,20 @@ class Car:
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int):
-
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car) -> int:
         return (car.comfort_class * (self.clean_power - car.clean_mark) *
                 self.average_rating / self.distance_from_city_center)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car) -> bool:
         return car.clean_mark > self.clean_power
 
-    def serve_cars(self, cars_list: list):
+    def serve_cars(self, cars_list: list) -> float:
         total_income = 0
         for car in cars_list:
             if self.wash_single_car(car):
@@ -31,8 +30,8 @@ class CarWashStation:
             car.clean_mark = self.clean_power
         return round(total_income, 1)
 
-    def rate_service(self, new_rating: float):
+    def rate_service(self, new_rating: float) -> None:
         self.count_of_ratings += 1
         self.average_rating = round(
-            ((self.average_rating * (self.count_of_ratings - 1)) +
+            ((self.average_rating * (self.count_of_ratings - 1)) + 
              new_rating) / self.count_of_ratings, 1)
